0c5e480dbf442596f6f10a38841bd63e702ec889
Noah Weber <webern2@winthrop.edu>
2016-03-23 21:35:13 -0400

    Configed LDAP integration, can now sign in and authenticate

---
+++ src/AppBundle/Controller/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+/* --------------------------------------------------
+    LoginController.php
+    Handles sign ins, authenticates users via LDAP
+    Author: Noah Weber
+------------------------------------------------------- */
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Form\LoginType;
+use Symfony\Component\Ldap\Exception\ConnectionException;
+
+class LoginController extends Controller
+{
+
+    /**
+     * Login controller.
+     *
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        $authUtils = $this->get('security.authentication_utils');
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+
+        return $this->render(
+            'login/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error
+            ));
+    }
+}
