5a2726bad17d49273d4b62c692d2082c7e3a8655
Mohammed Kashkari <mohammedkashkari@mohammeds-mbp.win.winthrop.edu>
2016-04-19 21:44:13 -0400

    Added comments to the specialRequest and donationRequest controllers

--- src/AppBundle/Controller/donationRequestController.php
+++ src/AppBundle/Controller/donationRequestController.php
@@ -31,6 +31,7 @@ class donationRequestController extends Controller
     {
         $donationRequest = new Inventory();
 
+        // Create the special request form
         $form = $this->createFormBuilder($donationRequest)
             ->add('itemType')
             ->add('itemColor')
@@ -42,16 +43,22 @@ class donationRequestController extends Controller
             ->getForm();
         $form->handleRequest($request);
 
+        // when the form is submitted and all fields are valid.
         if ($form->isSubmitted() && $form->isValid()) {
+            // set the status of the request to 'PENDING_DONATION'
             $Status = $this->getDoctrine()->getRepository('AppBundle:Status')->findOneBy(array('name'  => 'PENDING_DONATION'));
             $donationRequest->setItemStatus($Status);
+
+            // set the user of request to the user who is logged in.
             $User = $this->getDoctrine()->getRepository('AppBundle:User')->findOneBy(array('username'  => $this->getUser()));
             $donationRequest->setUser($User);
 
+            // put the request on the inventory table.
             $em = $this->getDoctrine()->getManager();
             $em->persist($donationRequest);
             $em->flush();
 
+            // send a notification email to the admin
             $message = \Swift_Message::newInstance()
                 ->setSubject('New Donation Request Submitted to WU Regalia Closet')
                 ->setFrom('wuregalia@gmail.com')
@@ -64,6 +71,7 @@ class donationRequestController extends Controller
                 );
             $this->get('mailer')->send($message);
 
+            // send a confirmation email to the admin
             $message = \Swift_Message::newInstance()
                 ->setSubject('Donation Request Confirmation')
                 ->setFrom('wuregalia@gmail.com')
@@ -76,9 +84,11 @@ class donationRequestController extends Controller
                 );
             $this->get('mailer')->send($message);
 
+            // go to the confirmation page
             return $this->render('donationRequest/success.html.twig');
         }
 
+        // show the form, including an error if it was submitted and not valid.
         return $this->render('donationRequest/new.html.twig', array(
             'form' => $form->createView(),
         ));
@@ -88,9 +98,9 @@ class donationRequestController extends Controller
      */
     public function adminAction(Request $request)
     {
+        // find all inventory records with a status of pending donation request
         $Status = $this->getDoctrine()->getRepository('AppBundle:Status')->findOneBy(array('name'  => 'PENDING_DONATION'));
         $donationRequestsList = $this->getDoctrine()->getRepository('AppBundle:Inventory')->findBy(array('itemStatus' => $Status ));
-
         return $this->render('donationRequest/admin.html.twig',array(
             'donationRequests' => $donationRequestsList
         ));
@@ -100,6 +110,7 @@ class donationRequestController extends Controller
      */
     public function adminChangeStatusAction(Request $request, $inventoryRequest)
     {
+        // set the status of the request to pending arrival when the donation request is accepted
         $em = $this->getDoctrine()->getManager();
         $record = $em->getRepository('AppBundle:Inventory')->find($inventoryRequest);
         $Status = $em->getRepository('AppBundle:Status')->findOneBy(array('name' => 'PENDING_ARRIVAL' ));
@@ -107,6 +118,7 @@ class donationRequestController extends Controller
 
         $em->flush();
 
+        // send an email to the user to let him/her know that the request has been accepted
         $message = \Swift_Message::newInstance()
             ->setSubject('Donation Request Accepted')
             ->setFrom('wuregalia@gmail.com')
@@ -119,6 +131,7 @@ class donationRequestController extends Controller
             );
         $this->get('mailer')->send($message);
 
+        // show all of the inventory records with a status of pending donation request
         $Status = $this->getDoctrine()->getRepository('AppBundle:Status')->findOneBy(array('name'  => 'PENDING_DONATION'));
         $donationRequestsList = $this->getDoctrine()->getRepository('AppBundle:Inventory')->findBy(array('itemStatus' => $Status ));
         return $this->render('donationRequest/admin.html.twig',array(
@@ -131,11 +144,13 @@ class donationRequestController extends Controller
      */
     public function adminRejectSpecialRequestAction(Request $request,$inventoryRequest)
     {
+        // delete the request when it is rejected
         $em = $this->getDoctrine()->getManager();
         $record = $em->getRepository('AppBundle:Inventory')->find($inventoryRequest);
         $em->remove($record);
         $em->flush();
 
+        // send an email to the user to let him/her know that the request has been rejected
         $message = \Swift_Message::newInstance()
             ->setSubject('Donation Request Rejected')
             ->setFrom('wuregalia@gmail.com')
@@ -148,6 +163,7 @@ class donationRequestController extends Controller
             );
         $this->get('mailer')->send($message);
 
+        // show all of the inventory records with a status of pending donation request
         $Status = $this->getDoctrine()->getRepository('AppBundle:Status')->findOneBy(array('name'  => 'PENDING_DONATION'));
         $donationRequestsList = $this->getDoctrine()->getRepository('AppBundle:Inventory')->findBy(array('itemStatus' => $Status ));
         return $this->render('donationRequest/admin.html.twig',array(
